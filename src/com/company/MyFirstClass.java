ac393b2f1eeac9d198959b69276838617c09fcfc
Dave Wolf <dave45678@gmail.com>
2017-10-11 09:32:35 -0400

    Initial Commit

---
+++ src/com/company/MyFirstClass.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        do{
+            System.out.print("* ");
+            i++;
+        }while (i < 10);
+
+    }
+}
